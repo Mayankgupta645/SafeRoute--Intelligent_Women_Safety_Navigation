63c84d1d8c5009eb26039511cec2c0121db571cd
Mayankgupta645 <mayank.gupta1.mm@gmail.com>
2026-01-12 02:31:49 +0530

    add a cloud data base

--- safe-route-backend/src/db/index.js
+++ safe-route-backend/src/db/index.js
@@ -1,14 +1,19 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "saferoute",
+//   password: "mayank",
+//   port: 5432,
+// });
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "saferoute",
-  password: "mayank",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
-
 pool.connect()
   .then(() => console.log("✅ PostgreSQL connected"))
   .catch(err => console.error("❌ DB connection error", err));
